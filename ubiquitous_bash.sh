7d3a36814144461f21bd7f75d488afa9bf3e3298
mirage335 <spamfreemirage335@gmail.com>
2023-08-15 00:48:03 -0400

    Draft.

--- ubiquitous_bash.sh
+++ ubiquitous_bash.sh
@@ -36,7 +36,7 @@ _ub_cksum_special_derivativeScripts_contents() {
 #export ub_setScriptChecksum_disable='true'
 ( [[ -e "$0".nck ]] || [[ "${BASH_SOURCE[0]}" != "${0}" ]] || [[ "$1" == '--profile' ]] || [[ "$1" == '--script' ]] || [[ "$1" == '--call' ]] || [[ "$1" == '--return' ]] || [[ "$1" == '--devenv' ]] || [[ "$1" == '--shell' ]] || [[ "$1" == '--bypass' ]] || [[ "$1" == '--parent' ]] || [[ "$1" == '--embed' ]] || [[ "$1" == '--compressed' ]] || [[ "$0" == "/bin/bash" ]] || [[ "$0" == "-bash" ]] || [[ "$0" == "/usr/bin/bash" ]] || [[ "$0" == "bash" ]] ) && export ub_setScriptChecksum_disable='true'
 export ub_setScriptChecksum_header='2591634041'
-export ub_setScriptChecksum_contents='661889244'
+export ub_setScriptChecksum_contents='1178610128'
 
 # CAUTION: Symlinks may cause problems. Disable this test for such cases if necessary.
 # WARNING: Performance may be crucial here.
@@ -17565,6 +17565,8 @@ _ubDistFetch_gitBestFetch() {
 	local currentCheckout
 	currentCheckout="$4"
 	[[ "$currentCheckout" == "" ]] && currentCheckout=HEAD
+
+	git config --global checkout.workers -1
 	
 	! _messagePlain_probe_cmd mkdir -p "$1"  && _core_FAIL
 	! _messagePlain_probe_cmd cd "$1" && _core_FAIL
