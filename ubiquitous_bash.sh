4afd816b93c0f169b027a87c88cb00ab99434b0c
mirage335 <spamfreemirage335@gmail.com>
2025-07-27 03:43:25 +0100

    Compile.

--- ubiquitous_bash.sh
+++ ubiquitous_bash.sh
@@ -39,7 +39,7 @@ _ub_cksum_special_derivativeScripts_contents() {
 #export ub_setScriptChecksum_disable='true'
 ( [[ -e "$0".nck ]] || [[ "${BASH_SOURCE[0]}" != "${0}" ]] || [[ "$1" == '--profile' ]] || [[ "$1" == '--script' ]] || [[ "$1" == '--call' ]] || [[ "$1" == '--return' ]] || [[ "$1" == '--devenv' ]] || [[ "$1" == '--shell' ]] || [[ "$1" == '--bypass' ]] || [[ "$1" == '--parent' ]] || [[ "$1" == '--embed' ]] || [[ "$1" == '--compressed' ]] || [[ "$0" == "/bin/bash" ]] || [[ "$0" == "-bash" ]] || [[ "$0" == "/usr/bin/bash" ]] || [[ "$0" == "bash" ]] ) && export ub_setScriptChecksum_disable='true'
 export ub_setScriptChecksum_header='3620520443'
-export ub_setScriptChecksum_contents='1518893782'
+export ub_setScriptChecksum_contents='3150666426'
 
 # CAUTION: Symlinks may cause problems. Disable this test for such cases if necessary.
 # WARNING: Performance may be crucial here.
@@ -26896,7 +26896,7 @@ _ubDistFetch() {
 	mkdir -p "$scriptLib"/core
 	cd "$scriptLib"/core
 
-	_ubDistFetch_gitBestFetch_github_mirage335-colossus "$scriptLib"/core cognition
+	#_ubDistFetch_gitBestFetch_github_mirage335-colossus "$scriptLib"/core cognition
 
 
 	#mkdir -p "$scriptLib"/infrastructure
@@ -27241,7 +27241,9 @@ _upgrade_sequence() {
     _upgrade_repository /home/user/core/infrastructure/ubDistBuild
     _upgrade_repository /home/user/core/infrastructure/ubDistFetch
 
-    _upgrade_repository /home/user/core/infrastructure/virtuousCritter
+    #_upgrade_repository /home/user/core/infrastructure/cognition
+
+    #_upgrade_repository /home/user/core/infrastructure/virtuousCritter
 
     _upgrade_repository /home/user/core/infrastructure/flightDeck
     _upgrade_repository /home/user/core/infrastructure/kinematicBase-large
